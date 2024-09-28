7f098611b8224e2f972ad53d4219e7454c531118
KeresztesHunor <kereszteshunor36@gmail.com>
2024-09-28 14:52:37 +0200

    quadratic matrix and beginnings of determinant calculation

--- Matrix.cs
+++ Matrix.cs
@@ -1,8 +1,8 @@
 ﻿namespace Matrices
 {
-    internal class Matrix(float[,] values)
+    internal class Matrix(float[,] values) : IEquatable<Matrix>
     {
-        float[,] matrix { get; } = values;
+        protected float[,] matrix { get; } = values;
 
         public int Width => matrix.GetLength(0);
         public int Height => matrix.GetLength(1);
@@ -12,13 +12,9 @@
             get
             {
                 float[,] values = new float[Height, Width];
-                for (int i = 0; i < Width; i++)
-                {
-                    for (int k = 0; k < Height; k++)
-                    {
-                        values[k, i] = matrix[i, k];
-                    }
-                }
+                Iterate(values, (int i, int k) => {
+                    values[k, i] = matrix[i, k];
+                });
                 return new Matrix(values);
             }
         }
@@ -33,14 +29,47 @@
 
         static void FillWith0(float[,] matrix)
         {
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            Iterate(matrix, (ref float value) => {
+                value = 0;
+            });
+        }
+
+        protected static void Iterate(int i, int k, Action<int, int> action)
+        {
+            for (int ii = 0; ii < i; ii++)
             {
-                for(int k = 0; k < matrix.GetLength(1); k++)
+                for (int kk = 0; kk < k; kk++)
                 {
-                    matrix[i, k] = 0;
+                    action(ii, kk);
                 }
             }
         }
+
+        protected static void Iterate(Matrix matrix, Action<int, int> action)
+        {
+            Iterate(matrix.Width, matrix.Height, action);
+        }
+
+        protected static void Iterate(float[,] values, Action<int, int> action)
+        {
+            Iterate(values.GetLength(0), values.GetLength(1), action);
+        }
+
+        protected static void Iterate(float[,] values, Action<float> action)
+        {
+            Iterate(values, (int i, int k) => {
+                action(values[i, k]);
+            });
+        }
+
+        protected static void Iterate(float[,] values, ReferentialAction action)
+        {
+            Iterate(values, (int i, int k) => {
+                action(ref values[i, k]);
+            });
+        }
+
+        protected delegate void ReferentialAction(ref float value);
 
         static Matrix PerformCommutativeOperation(Matrix left, Matrix right, Func<float, float, float> operation, Func<Matrix, Matrix, bool>? error = null, string? errorMessage = null)
         {
@@ -61,6 +90,15 @@
 
         static bool CheckSizes(Matrix left, Matrix right) => left.Width != right.Width || left.Height != right.Height;
 
+        static Matrix ArithmetizeNewMatrix(int width, int height, Action<float[,]> action)
+        {
+            float[,] values = new float[width, height];
+            action(values);
+            return new Matrix(values);
+        }
+
+        public bool Equals(Matrix? other) => matrix.Equals(other);
+
         public static Matrix operator +(Matrix left, Matrix right) => PerformCommutativeOperation(left, right, (left, right) => left + right, CheckSizes, "Cannot calculate sum of two matrices of differing sizes.");
 
         public static Matrix operator -(Matrix left, Matrix right) => PerformCommutativeOperation(left, right, (left, right) => left - right, CheckSizes, "Cannot calculate difference of two matrices of differing sizes.");
@@ -71,20 +109,24 @@
             {
                 throw new ArgumentException("Cannot multiply two matrices where the left matrix's height doesn't equal the right matrix's width and vice versa.");
             }
-            float[,] values = new float[right.Width, left.Height];
-            //For formality
-            //FillWith0(values);
-            for (int leftK = 0; leftK < left.Height; leftK++)
-            {
-                for (int rightI = 0; rightI < right.Width; rightI++)
+            return ArithmetizeNewMatrix(right.Width, left.Height, (float[,] values) => {
+                // For formality
+                //FillWith0(values);
+                for (int leftK = 0; leftK < left.Height; leftK++)
                 {
-                    for (int rightK = 0; rightK < right.Height; rightK++)
-                    {
+                    Iterate(right.Width, right.Height, (int rightI, int rightK) => {
                         values[rightI, leftK] += left[rightK, rightI] * right[rightI, rightK];
-                    }
+                    });
                 }
-            }
-            return new Matrix(values);
+            });
         }
+
+        public static Matrix operator *(Matrix matrix, float lambda) => ArithmetizeNewMatrix(matrix.Width, matrix.Height, (float[,] values) => {
+            Iterate(values, (ref float value) => {
+                value *= lambda;
+            });
+        });
+
+        public static Matrix operator *(float lambda, Matrix matrix) => matrix * lambda;
     }
 }
---
+++ QuadraticMatrix.cs
@@ -0,0 +1,39 @@
+﻿namespace Matrices
+{
+    internal class QuadraticMatrix : Matrix
+    {
+        Func<bool, float>? getDeterminant { get; set; }
+
+        public QuadraticMatrix(float[,] values) : base(values.GetLength(0) == values.GetLength(1) ? values : throw new ArgumentException("The width and height of a quadratic matrix has to be equal."))
+        {
+            getDeterminant = InitGetDeterminant();
+        }
+
+        public QuadraticMatrix(int n) : this(new float[n, n])
+        {
+
+        }
+
+        QuadraticMatrix(float[,] values, bool initGetDeterminant = false) : base(values)
+        {
+            getDeterminant = initGetDeterminant ? InitGetDeterminant() : null;
+        }
+
+        public float GetDeterminant(bool checkFor0RowOrColumn = true) => getDeterminant?.Invoke(checkFor0RowOrColumn) ?? 0;
+
+        Func<bool, float> InitGetDeterminant() => (bool checkFor0RowOrColumn) => {
+            bool calculateDeterminant = true;
+            if (checkFor0RowOrColumn)
+            {
+                
+            }
+            float determinant = 0;
+            if (calculateDeterminant)
+            {
+
+            }
+            getDeterminant = (bool _) => determinant;
+            return determinant;
+        };
+    }
+}
